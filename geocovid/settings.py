6e287ea5fd9868ffdef9a6a6f2bde399d8c2a636
diegofsousa <diegofernando5672@gmail.com>
2020-04-07 02:39:22 -0300

    [#16] Retinando sqlite e adicionando Postgres no docker-compose

--- geocovid/settings.py
+++ geocovid/settings.py
@@ -104,13 +104,24 @@ WSGI_APPLICATION = 'geocovid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+ """
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME_PRODUCTION'),
+        'USER': config('DB_USER_PRODUCTION'),
+        'PASSWORD': config('DB_PASSWORD_PRODUCTION'),
+        'HOST': config('DB_HOST_PRODUCTION'),
+        'PORT': config('DB_PORT_PRODUCTION'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
